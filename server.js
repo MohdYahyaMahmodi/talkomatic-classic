70afc216322e99981b59590fe6f1579bb5bac617
Mohd Mahmodi <141009456+MohdYahyaMahmodi@users.noreply.github.com>
2025-02-10 17:42:20 -0800

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'https://classic.talkomatic.co',
-  'https://dev.talkomatic.co/'
+  'https://dev.talkomatic.co'
 ];
 
 // CORS config
